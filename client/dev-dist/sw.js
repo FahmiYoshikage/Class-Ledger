dd3f437d78610078e596c78d05d21d50731c4518
FahmiYoshikage <fahmiilham029@gmail.com>
2025-10-30 00:39:33 +0700

    josjis

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.tt792bsbf08"
+    "revision": "0.538eg2am0ps"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
